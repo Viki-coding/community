c50aa169a78ace55cf59b7f3fd28b4167efd128e
Viki-coding <vikimulhall@gmail.com>
2025-02-03 13:58:33 +0000

    Install heruku, create profile, insert heroku in allowedhosts

--- facilitator/views.py
+++ facilitator/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
+
