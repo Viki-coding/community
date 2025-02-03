a4382842c8c6544c97d4ece30a6f090083fd542e
Viki-coding <vikimulhall@gmail.com>
2025-02-03 13:22:26 +0000

    Create a facilitator app, add app to settings. Update import paths in views.

---
+++ facilitator/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegistrationForm
