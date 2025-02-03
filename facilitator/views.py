d9cf0a9b4fa99e876f161b67743f8843e3fa2bcd
Viki-coding <vikimulhall@gmail.com>
2025-02-03 15:29:02 +0000

    Add facilitator home.html, add url pattern and path for same

--- facilitator/views.py
+++ facilitator/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.decorators import login_required
-from .models import Post
-from .forms import PostForm
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'facilitator/home.html')
+
+
+#from django.shortcuts import render, redirect, get_object_or_404
+#from django.contrib.auth import login, authenticate
+#from django.contrib.auth.forms import AuthenticationForm
+#from django.contrib.auth.decorators import login_required
+#from .models import Post
+#from .forms import PostForm
 
