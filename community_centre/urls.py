d3b436c53a6c5d7e38af910ef8d8f70c712054aa
Viki-coding <vikimulhall@gmail.com>
2025-02-06 12:19:29 +0000

    Debug to False prior to deploy

--- community_centre/urls.py
+++ community_centre/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('noticeboard.urls')),  # Include facilitator URLs
+    path('', include('noticeboard.urls')),  # Include noticeboard URLs
 ]
