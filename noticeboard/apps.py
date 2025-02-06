f3f97a6685df7b5b299265e1ee1c0d2cdd8ae881
Viki-coding <vikimulhall@gmail.com>
2025-02-06 10:32:01 +0000

    Rename facilitator app to more appropiate name noticeboard

--- noticeboard/apps.py
+++ noticeboard/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FacilitatorConfig(AppConfig):
+class NoticeboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'facilitator'
+    name = 'noticeboard'
--- noticeboard/views.py
+++ noticeboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'facilitator/home.html')
+    return render(request, 'noticeboard/home.html')
 
 
 
