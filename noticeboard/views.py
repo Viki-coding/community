fda87a828e7f6a8d1248dde0cbf65c8c0fca498b
Viki-coding <vikimulhall@gmail.com>
2025-02-06 14:16:00 +0000

    import events and eventforms in views.py

--- noticeboard/views.py
+++ noticeboard/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Post
 
+from django.views import generic
+from .models import Event
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import EventForm
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -9,7 +10,7 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'noticeboard/index.html'
     paginate_by = 3
-    
+
 def home(request):
     return render(request, 'noticeboard/home.html')
 
