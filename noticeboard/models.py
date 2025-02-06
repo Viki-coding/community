7b2bdf548e05994c243d0ab604273cde5070e13f
Viki-coding <vikimulhall@gmail.com>
2025-02-06 14:50:38 +0000

    Add location class to models

--- noticeboard/models.py
+++ noticeboard/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
 # Create your models here.
 class Event(models.Model):
     CATEGORY_CHOICES = [
@@ -19,7 +26,7 @@ class Event(models.Model):
     date = models.DateField()
     starttime = models.TimeField()
     endtime = models.TimeField()
-    location = models.CharField(max_length=200)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='Other')
     excerpt = models.TextField(blank=True)
     description = models.TextField()
--- noticeboard/views.py
+++ noticeboard/views.py
@@ -2,18 +2,19 @@
 from django.views import generic
 from .models import Event
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import EventForm
 
 # Create your views here.
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'noticeboard/index.html'
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'noticeboard/event_list.html', {'events': events})   
+
+class EventList(generic.ListView):
+    model = Event
+    queryset = Event.objects.order_by('-created')
+    template_name = 'noticeboard/event.html'
     paginate_by = 3
 
 def home(request):
-    return render(request, 'noticeboard/home.html')
-
-
+    return render(request, 'noticeboard/event.html')
 
 
