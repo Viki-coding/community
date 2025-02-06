3f7dee2087ffb5171c15351991a69ff1fc7ef9ce
Viki-coding <vikimulhall@gmail.com>
2025-02-06 13:28:37 +0000

    Add class based views to event

--- noticeboard/views.py
+++ noticeboard/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
+
+# Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'noticeboard/index.html'
+    paginate_by = 3
+    
 def home(request):
     return render(request, 'noticeboard/home.html')
 
