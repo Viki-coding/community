dfd2ca4a6395d018907a9ba8474b7e5a424a2c12
Viki-coding <vikimulhall@gmail.com>
2025-02-06 10:53:09 +0000

    create events model

--- noticeboard/models.py
+++ noticeboard/models.py
@@ -2,3 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    CATEGORY_CHOICES = [
+        ('5-Aside Soccer', '5-Aside Soccer'),
+        ('GAA', 'GAA'),
+        ('St. Olivers Ladies', 'St. Olivers Ladies'),
+        ('St. Olivers', 'St. Olivers'),
+        ('Finish Valley Athletics', 'Finish Valley Athletics'),
+        ('Community Centre', 'Community Centre'),
+        ('Yoga', 'Yoga'),
+        ('Parent & Toddler Group', 'Parent & Toddler Group'),
+        ('Other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='Other')
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
