9e6141cda6b051822bc02da350d8378b1ca7273f
Viki-coding <vikimulhall@gmail.com>
2025-02-07 15:33:40 +0000

    Update views.py file to fix template does not exist

--- noticeboard/views.py
+++ noticeboard/views.py
@@ -10,15 +10,10 @@ def event_list(request):
 
 class EventList(generic.ListView):
     model = Event
-    queryset = Event.objects.order_by('-created') #order in descending order from the most recent to the oldest
+    queryset = Event.objects.order_by('-created') # order in descending order from the most recent to the oldest
     template_name = 'noticeboard/index.html'
     paginate_by = 3
 
-# filepath: /Users/vikimulhall/Documents/community/noticeboard/views.py
-from django.shortcuts import render
-from .models import Event
-
 def home(request):
     events = Event.objects.all()
-    return render(request, 'index.html', {'events': events})
-
+    return render(request, 'noticeboard/index.html', {'events': events})
