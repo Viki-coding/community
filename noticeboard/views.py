44facfd6b5b254973207b23581816520b6bc723e
Viki-coding <vikimulhall@gmail.com>
2025-02-07 11:11:48 +0000

    Change views.py to point to index rather than events

--- noticeboard/views.py
+++ noticeboard/views.py
@@ -6,15 +6,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 def event_list(request):
     events = Event.objects.all()
-    return render(request, 'noticeboard/event_list.html', {'events': events})   
+    return render(request, 'noticeboard/index.html', {'events': events})  
 
 class EventList(generic.ListView):
     model = Event
     queryset = Event.objects.order_by('-created') #order in descending order from the most recent to the oldest
-    template_name = 'noticeboard/event.html'
+    template_name = 'noticeboard/index.html'
     paginate_by = 3
 
 def home(request):
-    return render(request, 'noticeboard/event.html')
+    return render(request, 'noticeboard/index.html')
 
 
