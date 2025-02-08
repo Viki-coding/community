21aa5cbd606e147a9e15f8038d934d7485b7f38b
Viki-coding <vikimulhall@gmail.com>
2025-02-08 15:45:23 +0000

    Delete summernotes and amdin imports from noticeboard.urls

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin    # Import admin module
 from django.urls import path
 from . import views
 
@@ -6,6 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),  # Root URL
     path('home/', views.home, name='home'),  # /home URL
     path('events/', views.EventList.as_view(), name='event_list'),
-    path('summernote/', include('django_summernote.urls')),
     path('login/', views.home, name='login'), 
 ]
