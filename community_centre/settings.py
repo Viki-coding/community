86fa0af1e6e50be49d87b27c2f688dd33b4b6d39
Viki-coding <vikimulhall@gmail.com>
2025-02-08 14:46:14 +0000

    Update static file directory in settings to fix heroku bug

--- community_centre/settings.py
+++ community_centre/settings.py
@@ -143,8 +143,9 @@ STATIC_URL = '/static/'
 # Add this if you have custom static files
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
