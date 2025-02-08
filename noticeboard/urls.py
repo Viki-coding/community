1810bd102efc918bf320ec662dbf743d904fe5bd
Viki-coding <vikimulhall@gmail.com>
2025-02-08 15:05:31 +0000

    Add url pattern to handle login

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),  # Root URL
     path('home/', views.home, name='home'),  # /home URL
     path('events/', views.EventList.as_view(), name='event_list'),
+    path('login/', views.home, name='login'), 
 ]
