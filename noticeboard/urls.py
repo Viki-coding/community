a262a26e0067bf157e78942092bfe9b45ff1b37f
Viki-coding <vikimulhall@gmail.com>
2025-02-10 16:36:26 +0000

    Add a url pattern for the event

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),  # /home URL
     path('events/', views.EventList.as_view(), name='event_list'),
     path('login/', views.home, name='login'), 
+    path('', views.index, name='index'),
+    path('event/<int:event_id>/', views.event_detail, name='event_detail'),
 ]
