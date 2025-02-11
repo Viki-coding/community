07944c5c34eb25110c59fd77461b043a17cb5b0a
Viki-coding <vikimulhall@gmail.com>
2025-02-11 17:09:28 +0000

    Add a url path to create an event, add create_event.html file and add html, relates to #2

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('events/', views.EventList.as_view(), name='event_list'),
     path('login/', views.login_view, name='login'),  # URL pattern for login_view
     path('event/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('create_event/', views.create_event, name='create_event'),
 ]
