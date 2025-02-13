6ab770268d6249e766826323f78f5390a8a680fc
Viki-coding <vikimulhall@gmail.com>
2025-02-13 20:43:59 +0000

    Add excerpt to forms.py, create a edit_event.html page, relates to #3

--- noticeboard/forms.py
+++ noticeboard/forms.py
@@ -4,4 +4,4 @@ from .models import Event
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'date', 'starttime', 'endtime', 'location', 'category', 'description']
+        fields = ['title', 'date', 'starttime', 'endtime', 'location', 'category', 'excerpt', 'description']
