0fcf1c508447cf1ef076a21e0b15a87a251c6ffb
Viki-coding <vikimulhall@gmail.com>
2025-02-13 21:12:52 +0000

    Create a delete_event.html page, relates to #4

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),  # URL pattern for login_view
     path('event/<int:event_id>/', views.event_detail, name='event_detail'),
     path('create_event/', views.create_event, name='create_event'),
+    path('edit_event/<int:event_id>/', views.edit_event, name='edit_event'),
+    path('facilitator_dashboard/', views.facilitator_dashboard, name='facilitator_dashboard'),
 ]
