df3a90cf284cd4ef2daa25458fb2d3a067c3a182
Viki-coding <vikimulhall@gmail.com>
2025-02-13 10:25:08 +0000

    Delete repeat error of adding a second contrib.auth

--- community_centre/settings.py
+++ community_centre/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.auth',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
