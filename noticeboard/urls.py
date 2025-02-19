bbdd5e1d2227f08d2f3c8b1a886a3002eb025f98
Viki-coding <vikimulhall@gmail.com>
2025-02-19 18:38:06 +0000

    Fix logout bug by inserting url pattern

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),  # Root URL
     path('events/', views.EventList.as_view(), name='event_list'),
     path('login/', views.login_view, name='login'),  # URL pattern for login_view
+    path('logout/', views.logout_view, name='logout'),  # URL pattern for logout_view
     path('event/<int:event_id>/', views.event_detail, name='event_detail'), # URL pattern for event_detail
     path('create_event/', views.create_event, name='create_event'), # URL pattern for create_event
     path('edit_event/<int:event_id>/', views.edit_event, name='edit_event'), # URL pattern for edit_event
