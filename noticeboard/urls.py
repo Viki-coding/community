4f80897c297782aadbcfe69bff6de82c4c6c9e15
Viki-coding <vikimulhall@gmail.com>
2025-02-24 12:37:11 +0000

    Format code using black, add register and book event to urlpatterns, relates to #11

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -6,9 +6,11 @@ urlpatterns = [
     path('events/', views.EventList.as_view(), name='event_list'),
     path('login/', views.login_view, name='login'),  # URL pattern for login_view
     path('logout/', views.logout_view, name='logout'),  # URL pattern for logout_view
+     path('register/', views.register, name='register'),  # URL pattern for register
     path('event/<int:event_id>/', views.event_detail, name='event_detail'), # URL pattern for event_detail
     path('create_event/', views.create_event, name='create_event'), # URL pattern for create_event
     path('edit_event/<int:event_id>/', views.edit_event, name='edit_event'), # URL pattern for edit_event
     path('delete_event/<int:event_id>/', views.delete_event, name='delete_event'),  # URL pattern for delete_event
+    path('book_event/<int:event_id>/', views.book_event, name='book_event'),
     path('facilitator_dashboard/', views.facilitator_dashboard, name='facilitator_dashboard'), # URL pattern for facilitator_dashboard
 ]
